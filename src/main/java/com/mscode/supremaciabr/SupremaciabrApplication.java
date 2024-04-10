34885951c0360b560931ee8b07959dc16350e38d
marcelomscode <marcelomscode@gmail.com>
2024-04-10 11:22:19 -0300

    Comecando o projeto

---
+++ src/main/java/com/mscode/supremaciabr/SupremaciabrApplication.java
@@ -0,0 +1,13 @@
+package com.mscode.supremaciabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremaciabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupremaciabrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mscode/supremaciabr/SupremaciabrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscode.supremaciabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupremaciabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
