2b1df0c4be0554b3485c51c3d3d1a73c91951428
marcelomscode <marcelomscode+mscodeblog@gmail.com>
2024-10-26 00:23:17 -0300

    reversao

--- src/main/java/com/mscode/SupremaciaApplication.java
+++ src/main/java/com/mscode/SupremaciaApplication.java
@@ -1,18 +1,7 @@
 package com.mscode;
 
-import com.mscode.consumer.MessageProducer;
-import com.mscode.dto.MembrosDTO;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.support.converter.DefaultClassMapper;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashMap;
-import java.util.Map;
 
 //@EnableRabbit
 @SpringBootApplication
